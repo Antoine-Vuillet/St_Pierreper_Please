f04fee23931283a51a2a7a3b17087bce8f17bbc9
Alexy Lafosse <149681987+XLA17@users.noreply.github.com>
2025-10-29 10:19:51 +0100

    Fix: tooltip + hell and paradise light + objects pos

--- st-pierreper-please/Script/game.gd
+++ st-pierreper-please/Script/game.gd
@@ -10,47 +10,26 @@ const cursor_hand = preload("uid://c0v0h3v2u6on5")
 const sinnerListScene: Array = [priest, hunter, clown, kid, prostitute]
 
 const sinnerPos = Vector2(500, 100)
-const cluesPosList = [Vector2(450, 390), Vector2(900, 390), Vector2(680, 450), Vector2(450, 510), Vector2(900, 510)]
-const tooltipTime = 0.3
 
 var currentSinnerIndex = 0
+var cluesPosList: Array
 var currentSinner
-var tooltip
 
 #@export var currentSinner: Node  #ouais pas ouf
 
 
 var start = false
 var canClick = true
-var isHovering = false
-var hoverTime = 0.0
-var lastMousePos = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	cluesPosList.append($Object1.position)
+	cluesPosList.append($Object2.position)
+	cluesPosList.append($Object3.position)
+	cluesPosList.append($Object4.position)
+	cluesPosList.append($Object5.position)
 	initSinner()
-	tooltip = $tooltip
-	tooltip.visible = false
-	move_child(tooltip, get_child_count() - 1)
-	var style = StyleBoxFlat.new()
-	style.bg_color = Color(0.1, 0.1, 0.1, 0.5)
-	style.set_corner_radius_all(8)
-	tooltip.add_theme_stylebox_override("panel", style)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if isHovering:
-		var currentMousePos = get_viewport().get_mouse_position()
-		if currentMousePos.distance_to(lastMousePos) > 1:
-			hoverTime = 0.0
-			lastMousePos = currentMousePos
-			tooltip.visible = false
-		else:
-			hoverTime += delta
-			if hoverTime >= tooltipTime:
-				tooltip.visible = true
-				tooltip.position = lastMousePos + Vector2(10, -20)
 
 func initSinner():
 	if currentSinner:
@@ -65,7 +44,8 @@ func initSinner():
 	for i in range(currentSinner.clue_list.size()):
 		var clue = currentSinner.clue_list[i].instantiate()
 		add_child(clue)
-		clue.position = cluesPosList[i]
+		
+		clue.position = cluesPosList[i] - clue.get_size() * clue.get_scale() / 2
 		clue.connect("gui_input", Callable(self, "_on_clue_gui_input").bind(i))
 		clue.connect("mouse_entered", Callable(self, "_on_clue_mouse_entered").bind(clue))
 		clue.connect("mouse_entered", Callable(self, "_on_mouse_entered"))
@@ -76,14 +56,17 @@ func nextSinner():
 	currentSinnerIndex += 1
 	initSinner()
 
+
 func _on_hell_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		print("Clic sur Hell")
+		nextSinner()
+		pass
 
 
 func _on_paradise_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		print("Clic sur Paradise")
+		nextSinner()
+		pass
 
 
 func _on_clue_gui_input(event: InputEvent, clueIndex: int) -> void:
@@ -94,26 +77,12 @@ func _on_clue_gui_input(event: InputEvent, clueIndex: int) -> void:
 		currentSinner.startTimeline(book)
 		canClick = true
 
+
 func _on_mouse_entered():
 	Input.set_custom_mouse_cursor(cursor_hand)
 
 func _on_mouse_exited():
 	Input.set_custom_mouse_cursor(null)
-
-
-func _on_clue_mouse_entered(clue: Control):
-	isHovering = true
-	hoverTime = 0.0
-	tooltip.get_node("tooltipLabel").text = clue.title
-	tooltip.custom_minimum_size = Vector2.ZERO
-	tooltip.queue_sort()
-	lastMousePos = get_viewport().get_mouse_position()
-	#Input.set_custom_mouse_cursor(cursor_hand)
-
-func _on_clue_mouse_exited():
-	isHovering = false
-	tooltip.visible = false
-	#Input.set_custom_mouse_cursor(null)
 
 
 func _on_info_paper_gui_input(event: InputEvent) -> void:
@@ -137,3 +106,19 @@ func _on_info_paper_gui_input(event: InputEvent) -> void:
 	
 		#papper.receive_data(currentSinner.name_s)
 		#print(currentSinner.name_s)
+
+
+func _on_hell_mouse_entered() -> void:
+	$HellLight.visible = true
+
+
+func _on_hell_mouse_exited() -> void:
+	$HellLight.visible = false
+
+
+func _on_paradise_mouse_entered() -> void:
+	$ParadiseLight.visible = true
+
+
+func _on_paradise_mouse_exited() -> void:
+	$ParadiseLight.visible = false
