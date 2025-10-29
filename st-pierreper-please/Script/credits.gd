4fa585a988a410def6c2d293ae3ebc6595b6b4b7
Antoine Vuillet <121035207+Antoine-Vuillet@users.noreply.github.com>
2025-10-29 10:44:46 +0100

    fix: fixed returning from the credits scene
    Was using preload but the scene was in cache so it needed load instead

--- st-pierreper-please/Script/credits.gd
+++ st-pierreper-please/Script/credits.gd
@@ -1,7 +1,7 @@
 extends Control
 
-@onready var game = preload("uid://drabf8nw3ysjw")
+@onready var menu = load("res://Scenes/main_menu/main_menu.tscn")
 
 func load_scene():
 	await get_tree().create_timer(0.2).timeout
-	get_tree().change_scene_to_packed(game)
+	get_tree().change_scene_to_packed(menu)
--- st-pierreper-please/Script/main_menu.gd
+++ st-pierreper-please/Script/main_menu.gd
@@ -1,5 +1,6 @@
 extends Control
 @onready var game = preload("uid://dwog1r6ydfcne") 
+@onready var credits = preload("uid://caq6304yniqdf") 
 
 func _on_start_button_down() -> void:
 	await get_tree().create_timer(1).timeout
@@ -8,3 +9,8 @@ func _on_start_button_down() -> void:
 
 func _on_exit_button_down() -> void:
 	get_tree().quit() # Replace with function body.
+
+
+func _on_credits_pressed() -> void:
+	await get_tree().create_timer(0.2).timeout
+	get_tree().change_scene_to_packed(credits)
